7ab967ac9f553c2548db6cb25ae559e3505cd0ae
HJacksons <jacksonherberts@gmail.com>
2023-09-25 16:13:06 +0200

    pass json over

--- app/VoiceToText/app/transcribeMagic.py
+++ app/VoiceToText/app/transcribeMagic.py
@@ -7,8 +7,7 @@ from helpers import InvalidAudioFormatError, TranscriptionError
 # Function to hold the magic
 def transcribe_magic(audio_file):
     # The user recording will be saved as a .wav file with name audio.wav in the audio_asset folder
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    # audio_file = os.path.join(script_dir, "audio_asset", "recorded_audio.wav")
+    save_path = "/app/transcription.json"
 
     if not audio_file.lower().endswith(".wav"):
         raise InvalidAudioFormatError("Audio file must be a .wav file")
@@ -19,8 +18,13 @@ def transcribe_magic(audio_file):
         transcription = output["text"]
         # return output["text"]
 
-        with open(os.path.join(script_dir, "transcription.json"), "w") as json_file:
+        with open(save_path, "w") as json_file:
             json.dump({"transcription": transcription}, json_file)
 
     except Exception as e:
         raise TranscriptionError(f"There was an error transcribing the audio file: {e}")
+
+
+
+
+# TODO: SAVE THE CACHED MODEL IN THE DOCKER CONTAINER, download the model if it doesn't exist, also image save it
--- tests/test_TextToVoice/test_TextToVoice.py
+++ tests/test_TextToVoice/test_TextToVoice.py
@@ -1,2 +1,20 @@
-def test_placeholder():
-    assert True
+import json
+
+# Simulate reading a JSON object from a file or API
+transcription_str = '{"text": "Hello, world!"}'
+
+# Parse the JSON-formatted string
+try:
+    transcription = json.loads(transcription_str)
+except json.JSONDecodeError:
+    print("Error decoding JSON")
+    exit(1)
+
+# Access the 'text' field
+try:
+    text = transcription["text"]
+except KeyError:
+    print("Key 'text' not found in JSON object")
+    exit(1)
+
+print(f"Extracted text: {text}")
