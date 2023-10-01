f88aea43dae9fa7b378b940c88ffed9e736fdbb9
HJacksons <jacksonherberts@gmail.com>
2023-10-01 22:12:47 +0200

    Refactor and add save_transcription fn

--- app/VoiceToText/app/transcribeApp.py
+++ app/VoiceToText/app/transcribeApp.py
@@ -1,31 +1,31 @@
-from flask import Flask, abort, request
+from flask import Flask, abort, request, jsonify
 from tempfile import NamedTemporaryFile
 import whisper
-from whisper import _download, _MODELS
 import torch
 import json
 import os
 import time
 
-torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+MODEL_PATH = "models/"
+SAVE_PATH = "/text-to-voice-app/"
+
+app = Flask(__name__)
 
 
-# Download the base model
 def model_exists(model_path, filename):
     """Check if the model file exists at the specified path."""
     return os.path.exists(os.path.join(model_path, filename))
 
 
-model_path = "models/"
-if not model_exists(model_path, "base.pt"):
-    _download(_MODELS["base"], model_path, False)
+def download_and_load_model(model_name, model_path, device):
+    """Download the model if not present and load it."""
+    if not model_exists(model_path, f"{model_name}.pt"):
+        whisper._download(whisper._MODELS[model_name], model_path, False)
+    return whisper.load_model(model_name, device=device)
 
-# Load the model:
-model = whisper.load_model("base", device=DEVICE)
-save_path = "/text-to-voice-app/"
 
-app = Flask(__name__)
+model = download_and_load_model("base", MODEL_PATH, DEVICE)
 
 
 @app.route("/")
@@ -41,35 +41,32 @@ def transcribe_magic():
     results = []
 
     for filename, handle in request.files.items():
-        # Create a temporary file.
-        temp = NamedTemporaryFile()
-
-        handle.save(temp)
-
-        result = model.transcribe(temp.name)
-
-        results.append(
-            {
+        with NamedTemporaryFile() as temp:
+            handle.save(temp.name)
+            result = model.transcribe(temp.name)
+            results.append({
                 "transcript": result["text"],
-            }
-        )
-        # Rename the old transcription.json file if it exists
-    if os.path.exists(f"{save_path}transcription.json"):
+            })
+
+    save_transcription(results)
+
+    return jsonify({"results": results})
+
+
+def save_transcription(results):
+    """Save transcribed results to a file and handle renaming of old files."""
+    if os.path.exists(f"{SAVE_PATH}transcription.json"):
         current_timestamp = int(time.time())
-        renamed_file = f"{save_path}transcription_{current_timestamp}.json"
-        os.rename(f"{save_path}transcription.json", renamed_file)
-    # Save to transcription.json
+        renamed_file = f"{SAVE_PATH}transcription_{current_timestamp}.json"
+        os.rename(f"{SAVE_PATH}transcription.json", renamed_file)
+
     try:
-        with open(f"{save_path}transcription.json", "w") as outfile:
+        with open(f"{SAVE_PATH}transcription.json", "w") as outfile:
             json.dump({"results": results}, outfile, indent=4)
-        #
-
     except Exception as e:
-        print("Error saving file:", e)
-    # This will be automatically converted to JSON.
-    return {"results": results}
+        print(f"Error saving file: {e}")
+        abort(500, description="Internal Server Error while saving transcription")
 
 
-# Using port 5000 for the transcribe service
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
