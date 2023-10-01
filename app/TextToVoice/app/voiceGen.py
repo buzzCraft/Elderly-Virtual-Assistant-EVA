9d24f449d4c1bc365ec28c7b01e15c502bc5e447
HJacksons <jacksonherberts@gmail.com>
2023-10-01 22:08:00 +0200

    code refactor

--- app/TextToVoice/app/voiceGen.py
+++ app/TextToVoice/app/voiceGen.py
@@ -1,43 +1,69 @@
 import json
-from transformers import AutoProcessor, BarkModel
-import soundfile as sf
 import os
 import time
+from transformers import AutoProcessor, BarkModel
+import soundfile as sf
 
-with open("/text-to-voice-app/transcription.json", "r") as json_file:
-    transcription = json.load(json_file)
 
-# Extract text from transcription json file
-text = transcription["results"][0]["transcript"]
-print(f"Processing text: {text}")
+def load_transcription_from_file(file_path):
+    """Load and return transcription from a JSON file."""
+    with open(file_path, "r") as json_file:
+        transcription = json.load(json_file)
+    return transcription["results"][0]["transcript"]
 
-model_path = ".models/"
-model_name = "suno/bark-small"
 
-# Check if the model exists locally
-if not os.path.exists(os.path.join(model_path, "config.json")):
-    # If not, download and save
-    processor = AutoProcessor.from_pretrained(model_name)
-    model = BarkModel.from_pretrained(model_name)
-    processor.save_pretrained(model_path)
-    model.save_pretrained(model_path)
-else:
-    # If yes, load from the local directory
-    processor = AutoProcessor.from_pretrained(model_path)
-    model = BarkModel.from_pretrained(model_path)
+def get_model_and_processor(model_name, model_path):
+    """Retrieve the model and processor. If not available locally, download them."""
+    if not os.path.exists(os.path.join(model_path, "config.json")):
+        # Download and save model and processor if they don't exist locally
+        processor = AutoProcessor.from_pretrained(model_name)
+        model = BarkModel.from_pretrained(model_name)
+        processor.save_pretrained(model_path)
+        model.save_pretrained(model_path)
+    else:
+        # Load model and processor from local directory
+        processor = AutoProcessor.from_pretrained(model_path)
+        model = BarkModel.from_pretrained(model_path)
+    return model, processor
 
-voice_preset = "v2/en_speaker_1"
 
-inputs = processor(
-    "text",
-    voice_preset=voice_preset,
-)
+def generate_audio_from_text(model, processor, text, voice_preset):
+    """Generate audio from the given text."""
+    inputs = processor(text, voice_preset=voice_preset)
+    audio_array = model.generate(**inputs)
+    return audio_array.cpu().numpy().squeeze()
 
-audio_array = model.generate(**inputs)
-audio_array = audio_array.cpu().numpy().squeeze()
-# sample_rate = model.generation_config.sample_rate
 
-save_dr = "/text-to-voice-app/"
-output_path = os.path.join(save_dr, f"outaudio{time.time()}.wav")
-sf.write(output_path, audio_array, 22050, "PCM_24")
-print(f"Audio saved to {output_path}")
+def save_audio_to_file(audio_array, sample_rate, directory):
+    """Save the generated audio to a WAV file."""
+    output_path = os.path.join(directory, f"outaudio{time.time()}.wav")
+    sf.write(output_path, audio_array, sample_rate, "PCM_24")
+    return output_path
+
+
+def main():
+    # Constants and paths
+    TRANSCRIPTION_FILE = "/text-to-voice-app/transcription.json"
+    MODEL_NAME = "suno/bark-small"
+    MODEL_PATH = ".models/"
+    SAVE_DIR = "/text-to-voice-app/"
+    VOICE_PRESET = "v2/en_speaker_1"
+    SAMPLE_RATE = 22050
+
+    # Extract text from transcription
+    text = load_transcription_from_file(TRANSCRIPTION_FILE)
+    print(f"Processing text: {text}")
+
+    # Get model and processor
+    model, processor = get_model_and_processor(MODEL_NAME, MODEL_PATH)
+
+    # Generate audio from text
+    audio_array = generate_audio_from_text(model, processor, text, VOICE_PRESET)
+
+    # Save the generated audio
+    output_path = save_audio_to_file(audio_array, SAMPLE_RATE, SAVE_DIR)
+    print(f"Audio saved to {output_path}")
+
+
+if __name__ == "__main__":
+    main()
