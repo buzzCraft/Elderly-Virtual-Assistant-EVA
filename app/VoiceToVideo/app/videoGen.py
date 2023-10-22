8bb3f868043799b39bb4182cac49aad0787b611b
HJacksons <jacksonherberts@gmail.com>
2023-10-22 21:31:02 +0200

    revertdockef

--- app/VoiceToVideo/app/videoGen.py
+++ app/VoiceToVideo/app/videoGen.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 app = Flask(__name__)
 
 IMG_PATH = "/sadTalker/photos/face.png"
-RESULT_DIR = "/VoiceToVideo-app"
+RESULT_DIR = "/SadTalker/results"
 
 
 @app.route("/receive_voice", methods=["POST"])
