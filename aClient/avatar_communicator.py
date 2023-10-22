363b6420fa0a2985fec909e26f918cadf911bbc6
HJacksons <jacksonherberts@gmail.com>
2023-10-22 19:32:45 +0200

    cd v2vapp

--- aClient/avatar_communicator.py
+++ aClient/avatar_communicator.py
@@ -3,12 +3,13 @@ import logging
 import os
 import time
 import wave
-
 import paramiko
 import pyaudio
 from dotenv import load_dotenv
 from pygame import mixer
 from scp import SCPClient
+from gtts import gTTS
+import speech_recognition as sr
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -32,6 +33,7 @@ SERVER_USERNAME = os.getenv("SERVER_USERNAME_ENV")
 SERVER_PATH_UP = os.getenv("SERVER_PATH_ENV_UP")
 SERVER_PATH_DOWN = os.getenv("SERVER_PATH_ENV_DOWN")
 SSH_PRIVATE_KEY_PATH = os.getenv("SSH_PRIVATE_KEY_PATH")
+mixer.init()
 
 
 # CHECK_ENDPOINT = "http://sgpu1.cs.oslomet.no:5004/check_audio"
@@ -45,6 +47,50 @@ def play_welcome_message():
 
     while mixer.music.get_busy():
         time.sleep(0.1)
+
+
+def speak(text, lang="en"):
+    tts = gTTS(text=text, lang=lang, slow=False)
+    filename = os.path.join(os.getcwd(), "voice.mp3")
+    tts.save(filename)
+
+    # Check if the file exists after saving
+    if not os.path.exists(filename):
+        logging.error(f"File {filename} was not created!")
+        return
+
+    mixer.music.load(filename)
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(0.1)  # Wait until the file finishes playing
+
+    # Release the file
+    mixer.music.unload()
+
+    # Safely attempt to remove the file
+    for _ in range(5):  # Let's try 5 times to delete the file
+        try:
+            os.remove(filename)
+            break
+        except PermissionError:
+            logging.warning(f"Attempt to delete {filename} failed. Retrying...")
+            time.sleep(1)  # Waiting for 1 second before retrying
+
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Listening...")
+        audio = recognizer.listen(source)
+        try:
+            return recognizer.recognize_google(audio).lower()
+        except sr.UnknownValueError:
+            print("Sorry, I couldn't understand that. Please try again.")
+            return None
+        except sr.RequestError:
+            print("API unavailable. Please check your internet connection.")
+            return None
 
 
 def send_file_to_server(recordedfilename):
@@ -98,35 +144,6 @@ def get_latest_bark_filename(timeout=240):  # Timeout in seconds
     return None
 
 
-# def download_response_from_server():
-#     """Download the response audio file from the server using SCP."""
-#     full_path_on_server = get_latest_bark_filename()
-#     if not full_path_on_server:
-#         return None
-#
-#     # Extract only the filename from the full path
-#     responsefilename = os.path.basename(full_path_on_server)
-#     logging.info(f"Attempting to download {responsefilename} from the server.")
-#     if os.path.exists(responsefilename):
-#         os.remove(responsefilename)
-#
-#     # Build source and destination paths for scp
-#     source = f"{SERVER_USERNAME}@{SERVER_HOST}:{SERVER_PATH_DOWN}/{responsefilename}"
-#     destination = f"./{responsefilename}"
-#
-#     try:
-#         subprocess.run(
-#             ["scp", "-i", SSH_PRIVATE_KEY_PATH, source, destination], check=True
-#         )
-#         logging.info(f"Downloaded {responsefilename} from the server.")
-#         return responsefilename
-#     except subprocess.CalledProcessError as e:
-#         logging.error(
-#             f"Error occurred while downloading {responsefilename} using scp: {e}"
-#         )
-#         return None
-
-
 def download_response_from_server():
     full_path_on_server = get_latest_bark_filename()
     if not full_path_on_server:
@@ -161,14 +178,40 @@ def download_response_from_server():
 
 def play_response_from_server(responsefilename):
     """Play the response audio file."""
-    mixer.init()
-    mixer.music.unload()
-    time.sleep(1)  # Wait for 1 second
+    mixer.music.unload()  # Unload any previously loaded sound
     mixer.music.load(responsefilename)
     mixer.music.play()
-
     while mixer.music.get_busy():
         time.sleep(0.1)
+
+
+# def download_video_from_server():
+#     """Download the response videofile from the server using SCP."""
+#     full_path_on_server = get_latest_bark_filename()
+#     if not full_path_on_server:
+#         return None
+#
+#     # Extract only the filename from the full path
+#     responsefilename = os.path.basename(full_path_on_server)
+#     logging.info(f"Attempting to download {responsefilename} from the server.")
+#     if os.path.exists(responsefilename):
+#         os.remove(responsefilename)
+#
+#     # Build source and destination paths for scp
+#     source = f"{SERVER_USERNAME}@{SERVER_HOST}:{SERVER_PATH_DOWN}/{responsefilename}"
+#     destination = f"./{responsefilename}"
+#
+#     try:
+#         subprocess.run(
+#             ["scp", "-i", SSH_PRIVATE_KEY_PATH, source, destination], check=True
+#         )
+#         logging.info(f"Downloaded {responsefilename} from the server.")
+#         return responsefilename
+#     except subprocess.CalledProcessError as e:
+#         logging.error(
+#             f"Error occurred while downloading {responsefilename} using scp: {e}"
+#         )
+#         return None
 
 
 def save_as_wav(recordfilename):
@@ -240,8 +283,11 @@ def record_and_save(recordfilename="recorded_audio.wav"):
             )
         os.remove(recordfilename)
 
-        choice = input("Do you want to record again? (y/n): ")
-        if choice != "y":
+        speak("Do you want to record again? Say yes or no.")
+        choice = recognize_speech()
+
+        if choice == "no":
+            speak("Exiting...")
             print("Exiting...")
             break
 
