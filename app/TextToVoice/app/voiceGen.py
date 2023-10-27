03a1402994465652e708949ed031ec04d3efb350
HJacksons <jacksonherberts@gmail.com>
2023-10-27 10:33:18 +0200

    add nvidia base imade

--- app/TextToVoice/app/voiceGen.py
+++ app/TextToVoice/app/voiceGen.py
@@ -22,7 +22,7 @@ silero_model, _ = torch.hub.load(
     speaker=model_id,
 )
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
 silero_model.to(device)  # gpu or cpu
 
 app = Flask(__name__)
