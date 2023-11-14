ae6c990e8984f3c7ac7af45ef965fd2525fe92d2
HJacksons <jacksonherberts@gmail.com>
2023-11-14 16:55:56 +0100

    easy app

--- aClient/app.py
+++ aClient/app.py
@@ -20,25 +20,27 @@ def process_audio():
     file_path = None
     feedback = None
 
-    # Handle initial audio data for transcription
     if "audio_data" in request.files:
         audio_file = request.files["audio_data"]
         audio_filename = f"{unique_id}_transcript.wav"
         audio_file.save(audio_filename)
 
-        # Send audio file to transcription service and get feedback
         try:
-            transcription_response = requests.post(
-                "http://transcribe:5000/receive_audio",
-                files={"audio_data": open(audio_filename, "rb")},
-                timeout=60,
-            )
-            transcription_response.raise_for_status()
+            with open(audio_filename, "rb") as audio_data:
+                transcription_response = requests.post(
+                    "http://transcribe:5000/receive_audio",
+                    files={"audio_data": audio_data},
+                    timeout=60,
+                )
+                transcription_response.raise_for_status()
             feedback = transcription_response.json().get("response", "")
         except requests.exceptions.RequestException as e:
+            logging.error(f"Error in transcription request: {e}")
+            os.remove(audio_filename)  # Cleanup if error occurs
             return jsonify({"error": str(e)}), 500
 
-    # Handle response file (if included)
+        os.remove(audio_filename)  # Cleanup after successful transcription
+
     if "response_file" in request.files:
         response_file = request.files["response_file"]
         response_filename = f"response_{unique_id}.wav"
@@ -47,6 +49,7 @@ def process_audio():
         file_path = f"/static/{response_filename}"
         logging.info(f"Saved response file to {save_path}")
         logging.info(f"Response file path: {file_path}")
+
     return jsonify({"feedback": feedback, "file_path": file_path})
 
 
