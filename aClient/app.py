24c7cf6831aa1cd59c5090a36805598d478fae9c
HJacksons <jacksonherberts@gmail.com>
2023-11-14 16:22:17 +0100

    load

--- aClient/app.py
+++ aClient/app.py
@@ -9,15 +9,6 @@ logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 
-def save_response_audio(response_file):
-    unique_id = str(uuid.uuid4())
-    response_filename = f"response_{unique_id}.wav"
-    save_path = os.path.join(app.static_folder, response_filename)
-    response_file.save(save_path)
-    logging.info(f"save path", save_path)
-    return f"/static/{response_filename}"
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -26,41 +17,35 @@ def index():
 @app.route("/process_audio", methods=["POST"])
 def process_audio():
     if "audio_data" not in request.files:
-        return jsonify({"error": "No audio file received"})
+        return jsonify({"error": "No audio file received"}), 400
 
+    # Handle initial audio data for transcription
     audio_file = request.files["audio_data"]
     unique_id = str(uuid.uuid4())
     audio_filename = f"{unique_id}.wav"
     audio_file.save(audio_filename)
 
-    # Send audio file to the specified server endpoint
+    # Send audio file to transcription service and get feedback
     files = {"audio_data": open(audio_filename, "rb")}
     try:
-        response = requests.post(
-            "http://transcribe:5000/receive_audio",
-            files=files,
-            timeout=60,  # Set a timeout of 60 seconds
+        transcription_response = requests.post(
+            "http://transcribe:5000/receive_audio", files=files, timeout=60
         )
-        response.raise_for_status()
-        feedback = response.json().get("response", "")
-    except requests.Timeout:
-        logging.info("Error: Request to v2t timed out")
-        feedback = "Error: The request to v2t timed out"
-    except requests.RequestExceptiona as e:
-        logging.error(f"Request failed: {e}")
-        feedback = f"Error: {e}"
+        transcription_response.raise_for_status()
+        feedback = transcription_response.json().get("response", "")
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
 
+    # Handle response file (if included)
+    file_path = None
     if "response_file" in request.files:
         response_file = request.files["response_file"]
-        file_path = save_response_audio(response_file)
-        logging.info(f"Received response file saved as: {response_file.filename}")
-        logging.info(f"Sending file path to client: {file_path}")
+        response_filename = f"response_{unique_id}.wav"
+        save_path = os.path.join(app.static_folder, response_filename)
+        response_file.save(save_path)
+        file_path = f"/static/{response_filename}"
 
-        return jsonify(
-            {"status": "success", "message": "File received", "file_path": file_path}
-        )
-
-    return jsonify({"feedback": feedback})
+    return jsonify({"feedback": feedback, "file_path": file_path})
 
 
 if __name__ == "__main__":
