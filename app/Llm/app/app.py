c11c7a17df37ce49e9bdf8d9ada3a848da3db645
HJacksons <jacksonherberts@gmail.com>
2023-11-15 19:08:52 +0100

    styling the chat logs interface

--- app/Llm/app/app.py
+++ app/Llm/app/app.py
@@ -62,14 +62,14 @@ def generate_response():
         response = chatbot(user_input)
         chatbot_response = response.get("text", "")
         chatbot_response = re.sub(
-            r"^\w+(EVA|AI)\s*", "", chatbot_response
+            r"^\w+:\s*", "", chatbot_response
         )  # Remove the EVA and AI prefix
         chatbot_response = re.sub(
             r"\*.*?\*", "", chatbot_response
         )  # Remove the *emphasis*
-        chatbot_response = re.sub(
-            r"[^A-Za-z .]", "", chatbot_response
-        )  # Remove all non-alphabetical characters
+        # chatbot_response = re.sub(
+        # r"[^A-Za-z .]", "", chatbot_response
+        # )  # Remove all non-alphabetical characters
         chatbot_response = (
             chatbot_response.strip()
         )  # Remove leading and trailing whitespace
